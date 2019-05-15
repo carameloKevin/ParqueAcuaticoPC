109a3142d54578816ea071669ad8d46bbc17a253
caramelokevin <bergkevin1996@gmail.com>
2019-05-15 09:13:18 -0300

    Primera version de tirarsePorTobogan. Use las tres formas, creo que no es la mejor manera

--- src/parqueacuatico/FaroMirador.java
+++ src/parqueacuatico/FaroMirador.java
@@ -3,6 +3,9 @@ package parqueacuatico;
 import java.util.LinkedList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 //import java.util.concurrent.SynchronousQueue;
 /**
  *
@@ -12,21 +15,28 @@ import java.util.concurrent.locks.ReentrantLock;
     public class FaroMirador{
     /*La escalera es una linkedList para poder implementar sincronizacion alrededor
     Podria haber usar la SynchronousQueue*/
-    private LinkedList escalera;
-    /*Uso un integer porque creo que seria mejor solo verificar si toboganesOcupados != 2
-    en vez de hacer la clase y poner ocupado (es casi lo mismo)*/ 
-    private Integer tobaganesOcupados;
+    private LinkedList escalera;  
     private Lock subirEscaleraLock;
+    //Cada semaforo representa un tobogan
+    private Semaphore toboganes;
+    
+    /*
+    Decidi hacerlo con Semaphore porque queda medio feo con integer
+    /*Uso un integer porque creo que seria mejor solo verificar si toboganesOcupados != 2
+    en vez de hacer la clase y poner ocupado (es casi lo mismo) / 
+    private Integer tobaganesOcupados;
+    */
     
     public FaroMirador() {
         
         this.escalera = new LinkedList();
-        this.tobaganesOcupados = 0;
+        this.toboganes = new Semaphore(2);
         subirEscaleraLock = new ReentrantLock();
     }
     
     public void subirEscalera(Visitante unVisit)
     {
+        //Implementado con LOCK
         //Se sube un visitante a la escalera del faro
         System.out.println("FARO - Visitate Nro " + unVisit.getNombre() +" INTENTA subir al faro");
         subirEscaleraLock.lock();
@@ -41,9 +51,42 @@ import java.util.concurrent.locks.ReentrantLock;
         }
     }
     
-    public void tirarsePorTobogan(Visitante unVisit)
+    public synchronized void intentoTirarTobogan(Visitante unVisit)
     {
+        /*!!!!!!Tengo problemas con esto. Si un visitante quiere tirarse, tengo que verificar que sea el primero. Si no lo es
+        tiene qu esperar, el tema es que la unica forma que conozco es con el metodo wait, o sea monitores
+        */
         
+        //Verifico que el visitante que se quiere tirar sea el primero en la fila
+        while(!(escalera.getFirst() != null && escalera.getFirst() == unVisit))
+        {
+            //Si no es el primero, espera.
+            try{
+                //Notifico a otro que vea si el puede tirarse
+                this.notify();
+                //Pongo a dormir este thread
+                this.wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(FaroMirador.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        //Se tira por el tobogan
+        //!!NOTA - Yo puse un el metodo Thread.Sleep() aca dentro. Cuando va a este metodo, sigue el syncronized? mas que nada por el tema que no haga un Sleep mientras esta sincronizado
+        this.tirarseTobogan(unVisit);
+        
+        
+    }
+    
+    public void tirarseTobogan(Visitante unVist)
+    {
+        try {
+            //Solo toma un semaforo, simula que se tira y suelta el semaforo
+            toboganes.acquire();
+            Thread.sleep(200);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(FaroMirador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        toboganes.release();
     }
         
     }
