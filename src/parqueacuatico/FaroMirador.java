bbd4ff6346bcab5804958ab79299df42bdec81e0
caramelokevin <caramelokevin@CK-S55-B>
2020-02-03 11:40:36 -0300

    FaroMirador Nueva cola FIFO
    Cambie la linkedList (que no funcionaba) por un semaforo. Es feo, pero
    funcia bien y no creo que vaya a ser mucho problem. Falta hacer que
    heche a todo el mundo a las 17.

--- src/parqueacuatico/FaroMirador.java
+++ src/parqueacuatico/FaroMirador.java
@@ -1,6 +1,7 @@
 
 package parqueacuatico;
 import java.util.LinkedList;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,7 +14,7 @@ import java.util.logging.Logger;
     public class FaroMirador{
     /*La escalera es una linkedList para poder implementar sincronizacion alrededor
     Podria haber usar la SynchronousQueue*/
-    private LinkedList escalera;  
+    private SynchronousQueue<Visitante> escalera;  
     //Cada semaforo representa un tobogan
     private Semaphore toboganes,subirEscalera, mutex;
     
@@ -26,10 +27,9 @@ import java.util.logging.Logger;
     
     public FaroMirador() {
         
-        this.escalera = new LinkedList();
+        //this.escalera = new SynchronousQueue<Visitante>(true); //true para que sea FIFO
         this.toboganes = new Semaphore(2,true);
         this.subirEscalera = new Semaphore(1,true);
-        this.mutex = new Semaphore(1);
     }
     
     public void realizarFaroMirador(Visitante unVisitante){
@@ -44,6 +44,7 @@ import java.util.logging.Logger;
     
     public void subirEscalera(Visitante unVisit)
     {
+    	
         //Se sube un visitante a la escalera del faro
         System.out.println(unVisit.getNombreCompleto() + " FARO - Visitate Nro " + unVisit.getNombreCompleto() +" INTENTA subir al faro");
         try {
@@ -52,8 +53,9 @@ import java.util.logging.Logger;
             Logger.getLogger(FaroMirador.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        System.out.println(unVisit.getNombreCompleto() + " DEBUG - FARO - Visitate Nro " + unVisit.getNombreCompleto() +" TOMO el semaforo exitosamente");
-        
+        System.out.println(unVisit.getNombreCompleto() + " DEBUG - FARO - Visitate Nro " + unVisit.getNombreCompleto() +" TOMO el semaforo exitosamente<-------Esclera");
+       
+        /*
         try{
             //Se sube a la escalera
             escalera.add(unVisit);
@@ -61,6 +63,7 @@ import java.util.logging.Logger;
             System.out.println(unVisit.getNombreCompleto() + " DEBUG - FARO - Visitante Nro " + unVisit.getNombreCompleto() + "  LOGRO subir y SOLTO el Lock exitosamente");
             subirEscalera.release();
         }
+        */
     }
     
     public void intentoTirarTobogan(Visitante unVisit)
@@ -71,18 +74,25 @@ import java.util.logging.Logger;
         
         //Verifico que el visitante que se quiere tirar sea el primero en la fila
         //Verifico en un metodo afuera para evitar hacer el synchronized con el Thread.sleep() de tirarseTobogan
-        System.out.println(unVisit.getNombreCompleto() + " FARO - Soy el primero para tirarme?");
-        verificarTobogan(unVisit);
+        //System.out.println(unVisit.getNombreCompleto() + " FARO - Soy el primero para tirarme?");
+        //verificarTobogan(unVisit);
         
         //Se tira por el tobogan
         //!!NOTA - Yo puse un el metodo Thread.Sleep() aca dentro. Cuando va a este metodo, sigue el syncronized? mas que nada por el tema que no haga un Sleep mientras esta sincronizado
-        System.out.println(unVisit.getNombreCompleto() + " FARO - Es mi turno de tirarme!");
+        subirEscalera.release();
+    	System.out.println(unVisit.getNombreCompleto() + " FARO - Es mi turno de tirarme!");
         tirarseTobogan(unVisit);
         
         
     }
 
     private synchronized void verificarTobogan(Visitante unVisit){
+    	/**
+    	 * OBSOLETO. Lo que haciamos antes era poner una LinkedList en donde se iban agregando los que se subian a la escalera, pero es horrible
+    	 * que cada visitante tenga que ver si es su turno de tirarse, asi que lo remplazamos con un semaforo
+    	 */
+    	
+    	/*
             while(!(escalera.getFirst() != null && escalera.getFirst() == unVisit))
         {
             //Si no es el primero, espera.
@@ -96,25 +106,24 @@ import java.util.logging.Logger;
                 Logger.getLogger(FaroMirador.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        */
     }
     
     private void tirarseTobogan(Visitante unVisit)
     {
         try {
             //Solo toma un semaforo, simula que se tira y suelta el semaforo
+        	
             System.out.println(unVisit.getNombreCompleto() + " FARO - Intento obtener el semaforo del tobogan");
             toboganes.acquire();
-            //Lo quito de la cola de la escalera
-            mutex.acquire();
-                escalera.remove(unVisit);
-            mutex.release();
-            System.out.println(unVisit.getNombreCompleto() + " FARO - Obtuve el semaforo del tobogan, me tiro /duermo 200 mili <---------------------");
-            Thread.sleep(200);
+            
+            System.out.println(unVisit.getNombreCompleto() + " FARO - Obtuve el semaforo del tobogan, me tiro");
+            Thread.sleep(100);
         } catch (InterruptedException ex) {
             Logger.getLogger(FaroMirador.class.getName()).log(Level.SEVERE, null, ex);
         }
         toboganes.release();
-        System.out.println(unVisit.getNombreCompleto() + " FARO - Ya me tire y termine, solte el semaforo ----------------->");
+        System.out.println(unVisit.getNombreCompleto() + " FARO - Ya me tire y termine");
     }
         
     }
